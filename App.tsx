073063bf9e27c6ba9e6a2780c647816ec0dad7f6
Camilo <camilovillalobos252@gmail.com>
2025-08-19 20:51:29 -0500

    primer ejemplo calculadora

---
+++ App.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Switch} from 'react-native';
+import { ThemeContext } from './src/context/ThemeContext';
+import { mycolors } from './src/style/colores';
+import Button from './src/components/Button';
+
+export default function App() {
+  const [theme, setTheme] = useState('light');
+
+  return (
+    <ThemeContext.Provider value ={theme}>
+    <View style={theme === 'light' ? styles.container : [styles.container, {backgroundColor: '#000'}]}>
+      <Text style={theme === 'light' ? estiloLetra.ejemplo : [estiloLetra.ejemplo, {color: mycolors.white}]}>aprendiendo</Text>
+      <Text style={theme === 'light' ? estiloLetra.ejemplo : [estiloLetra.ejemplo, {color: mycolors.white}]}>aprendiendo</Text>
+      <StatusBar style="auto" />
+      <Switch value ={theme === 'light'} onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+      />
+      <Button isBlue title='3' onPress={() => {alert('hello')}} />
+    </View>
+    </ThemeContext.Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: mycolors.light,
+    alignItems: 'center',
+    color: mycolors.white,
+    justifyContent: 'center',
+  },
+});
+
+const estiloLetra = StyleSheet.create({
+  ejemplo: {
+    color: mycolors.black
+  }
+})
---
+++ src/style/colores.tsx
@@ -0,0 +1,12 @@
+export const mycolors = {
+    light: '#f1f2f3',
+    dark: '#17171c',
+    blue: '#0060baff',
+    btnGray: '#4e505f',
+    btnDark: '#2e2f38',
+    gray: '#747477',
+    black: '#000000',
+    white: '#ffffff',
+    result: '#46d5b2',
+
+}
